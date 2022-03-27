f1ecf318286a2b67bb5fd725fb2e3a80247e2b9c
Ishola Azeez <isholaazeezayo@gmail.com>
2022-03-27 11:46:55 +0100

    ERC20token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 contract DappToken {
     // constructor
     uint256 public totalSupply; //this is from ERC20
-    constructor() {
-        totalSupply = 1000000;
+    constructor(uint256 _totalSupply) {
+        totalSupply = _totalSupply;
     }
 }
