532fe8c96cd97c5b1bf464638a347ae7d40581a9
Ishola Azeez <isholaazeezayo@gmail.com>
2022-03-27 12:33:57 +0100

    ERC20token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,8 +2,13 @@ pragma solidity ^0.8.0;
 
 contract DappToken {
     // constructor
-    uint256 public totalSupply; //this is from ERC20
+    string public name = "LALA"; //this is for ERC20
+    mapping(address => uint256) public balanceOf; //this is for ERC20
+    uint256 public totalSupply; //this is for ERC20
+    string public symbol = "LL"; //this is for ERC20
+    string public standard = "LL token v1.0"; //this is for ERC20
     constructor(uint256 _totalSupply) {
         totalSupply = _totalSupply;
+        balanceOf[msg.sender] = totalSupply;
     }
 }
--- test/dapptoken.test.js
+++ test/dapptoken.test.js
@@ -5,9 +5,29 @@ contract("DappToken", (account) => {
     before(async () => {
         instance = await DappToken.deployed()
     })
+    it("should be named LALA", async () => {
+        let name = await instance.name()
+        let nm = await name
+        assert.equal(nm, "LALA", "the contract name should be LALA")
+    })
+    it("should be symbol LL", async () => {
+        let symbol = await instance.symbol()
+        let sym = await symbol
+        assert.equal(sym, "LL", "the contract symbol should be LL")
+    })
+    it("should be standard LL token v1.0", async () => {
+        let standard = await instance.standard()
+        let std = await standard
+        assert.equal(std, "LL token v1.0", "the contract symbol should be LL token v1.0")
+    })
     it("should check for total supply", async () => {
         let totalSupply = await instance.totalSupply()
         let ts = await totalSupply
         assert.equal(ts, 1000000, `the total supply should be 1000000`)
+    })
+    it("should return the balance", async () => {
+        let balance = await instance.balanceOf(account[0]);
+        let balanceOf = await balance
+        assert.equal(balanceOf, 1000000, "it should assign all the token to the deployer account")
     })
 })
