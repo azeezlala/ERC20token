8a114711052b028c18d250278d2ecbf684caa285
Ishola Azeez <isholaazeezayo@gmail.com>
2022-03-28 00:15:50 +0100

    ERC20token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,8 +7,28 @@ contract DappToken {
     uint256 public totalSupply; //this is for ERC20
     string public symbol = "LL"; //this is for ERC20
     string public standard = "LL token v1.0"; //this is for ERC20
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
     constructor(uint256 _totalSupply) {
         totalSupply = _totalSupply;
         balanceOf[msg.sender] = totalSupply;
+    }
+    // Transfer //this is for ERC20
+    // Exception if account doesn't have enough
+    // Return a boolean
+    // Transfer Event
+    modifier checkBalance (uint256 _value) {
+        require(balanceOf[msg.sender] >= _value, "Don't have enough token to proceed");
+        _;
+    }
+    function transfer(address _to, uint256 _value) public checkBalance(_value) returns(bool success) { //this is for ERC20 token
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+        //Transfer balance
     }
 }
--- test/dapptoken.test.js
+++ test/dapptoken.test.js
@@ -30,4 +30,23 @@ contract("DappToken", (account) => {
         let balanceOf = await balance
         assert.equal(balanceOf, 1000000, "it should assign all the token to the deployer account")
     })
+    it("should not tranfer token ownership", async () => {
+        try {
+            let token = await instance.transfer.call(account[1], 99999999999);//.call does not trigger a transaction, will not get a transaction info
+            let tk = await token
+            assert.fail()
+        }catch (e) {
+            assert(e.message.indexOf('revert') >= 0, "error message must revert");
+            let receipt = await instance.transfer(account[1], 250000, { from: account[0] });
+            assert.equal(receipt.logs.length, 1, "trigger one event")
+            assert.equal(receipt.logs[0].event, 'Transfer', "should be the 'Transfer' event")
+            assert.equal(receipt.logs[0].args._from, account[0], 'sender')
+            assert.equal(receipt.logs[0].args._to, account[1], 'from')
+            assert.equal(receipt.logs[0].args._value, 250000,'sent amount')
+            let balance = await instance.balanceOf(account[1]);
+            assert.equal(balance.toNumber(), 250000, "add the amount to the receiving account");
+            let ownerBalance = await instance.balanceOf(account[0]);
+            assert.equal(ownerBalance.toNumber(), 750000, "deduct amount from owner");
+        }
+    })
 })
